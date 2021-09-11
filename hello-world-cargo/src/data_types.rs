9fdc31454d2961a9cd440f685e654ffe7d0b5c78
hossein-nemati <80382370+hossein-nemati@users.noreply.github.com>
2021-09-11 18:57:13 +0430

    arrays

--- hello-world-cargo/src/data_types.rs
+++ hello-world-cargo/src/data_types.rs
@@ -137,4 +137,16 @@ pub fn data_types() {
 
     let x = true;
     println!("x is {}", x);
+
+    // Arrays
+
+    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+
+    for n in numbers.iter() {
+        println!("{}", n);
+    }
+
+    for i in 0..numbers.len() {
+        println!("{}", numbers[i]);
+    }
 }
